69b87adfa86d9db03af9606255df4f090b1af46e
obed vega <vega.obed@gmail.com>
2023-08-19 22:04:21 -0700

    mensajes

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function startLoading() {
+    var progressBar = document.getElementById("progress-bar");
+    var width = 0;
+    var interval = setInterval(function() {
+      if (width >= 100) {
+        clearInterval(interval);
+        progressBar.classList.add("loading-complete");
+      } else {
+        width++;
+        progressBar.style.width = width + "%";
+      }
+    }, 100); // Adjust the speed of the progress here
+  }
+s
+//  window.onload = 
+
+  function postData() {
+  //  alert('test');
+    var mensaje = document.getElementById("message");
+    var input = document.getElementById("siteAuditForm").value;
+    //alert(input);
+    if(input!==""){
+      var progressBar1 = document.getElementById("bar");
+      var progressBar2 = document.getElementById("progress-bar");
+      progressBar1.classList.remove("show");
+      progressBar2.classList.remove("show");
+      mensaje.classList.add("show0");
+
+      startLoading();
+    }else{
+      mensaje.classList.remove("show0");
+    //  alert('input vacio');
+    }     
+  }

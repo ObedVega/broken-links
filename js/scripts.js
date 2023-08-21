f8e7ba67b8979a6cd2ad4490606fb44b8e4cefc8
obed vega <vega.obed@gmail.com>
2023-08-21 11:47:37 -0700

    funcionalidad

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,91 @@
-function startLoading() {
-    var progressBar = document.getElementById("progress-bar");
-    var width = 0;
-    var interval = setInterval(function() {
+async function startLoading() {
+  var progressBar = document.getElementById("progress-bar");
+  var width = 0;
+
+  await new Promise(resolve => {
+    function incrementProgress() {
       if (width >= 100) {
         clearInterval(interval);
         progressBar.classList.add("loading-complete");
+        resolve(); // Resolve the promise when loading is complete
       } else {
         width++;
         progressBar.style.width = width + "%";
       }
-    }, 100); // Adjust the speed of the progress here
-  }
-s
-//  window.onload = 
+    }
 
-  function postData() {
+    var interval = setInterval(incrementProgress, 100); // Adjust the speed of the progress here
+  });
+  }
+
+
+
+  async function postData() {
   //  alert('test');
     var mensaje = document.getElementById("message");
-    var input = document.getElementById("siteAuditForm").value;
+    var url = document.getElementById("siteAuditForm").value;
     //alert(input);
-    if(input!==""){
+    if(url!==""){
       var progressBar1 = document.getElementById("bar");
       var progressBar2 = document.getElementById("progress-bar");
       var result = document.getElementById("results");
       progressBar1.classList.remove("show");
       progressBar2.classList.remove("show");
       mensaje.classList.add("show0");
-      result.classList.remove("show1");
+    
 
-      startLoading();
+      await startLoading();
+      
+      await getData(result, url);
     }else{
       mensaje.classList.remove("show0");
     //  alert('input vacio');
     }     
+  }
+  async function showLoader() {
+    document.getElementById("loader").style.display = "block";
+  }
+  
+  function hideLoader() {
+    document.getElementById("loader").style.display = "none";
+  }
+  
+  async function getData(result, url){
+    result.classList.remove("show1");
+    await showLoader();
+  //  alert(url);
+  //  url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+    .then((response) => {
+    //  console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      hideLoader();
+      let authors = data;
+      let listGroup = document.getElementById("resultados");
+
+      let listItems = authors.map(author => {
+        let { name, email } = author;
+        return `
+          <li class="list-group-item listext">
+            <span class="reddot"></span>
+            <span class="textrs">${name}</span>
+            <span class="flex-spacer"></span>
+            <button type="button" class="btn btn-outline-danger" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+              </svg>
+              View issue
+            </button>
+          </li>
+        `;
+      });
+
+      listGroup.innerHTML = listItems.join('');
+    })
+    .catch(error => {
+      hideLoader();
+      console.error('Error:', error);
+    });
   }

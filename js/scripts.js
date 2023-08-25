8dbb5720db6e2fe7991a010698c30a0ca5ba1a9a
obed vega <vega.obed@gmail.com>
2023-08-25 13:47:03 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ async function startLoading() {
       }
     }
 
-    var interval = setInterval(incrementProgress, 100); // Adjust the speed of the progress here
+    var interval = setInterval(incrementProgress, 20); // Adjust the speed of the progress here
   });
   }
 
@@ -29,13 +29,14 @@ async function startLoading() {
       var progressBar1 = document.getElementById("bar");
       var progressBar2 = document.getElementById("progress-bar");
       var result = document.getElementById("results");
+      var mensaje2 = document.getElementById("m2");
       progressBar1.classList.remove("s2");
       progressBar2.classList.remove("s2");
       mensaje.classList.add("s0");
     
 
       await startLoading();
-      
+      mensaje2.classList.remove("s3");
       await getData(result, url, lan);
     }else{
       mensaje.classList.remove("s0");
@@ -53,9 +54,10 @@ async function startLoading() {
   async function getData(result, url, lan){
     result.classList.remove("s1");
     await showLoader();
-  //  alert(url);
-  //  url = 'https://jsonplaceholder.typicode.com/users';
-    let api = `https://django-wv7q.onrender.com/api/check/${url}/`
+
+    var linkWithoutProtocol = url.replace("https://", "");
+
+    let api = `https://django-wv7q.onrender.com/api/check/${linkWithoutProtocol}/`
     fetch(api)
     .then((response) => {
     //  console.log(response);
@@ -66,16 +68,16 @@ async function startLoading() {
       let issue = 'View issue'
       let resultado = data;
       let listGroup = document.getElementById("resultados");
-      alert(lan);
+    //  alert(lan);
       if (lan==='ja'){
         issue = '問題を見る';
       }
       let listItems = resultado.map(res => {
-        let { nombre } = res;
+        let { ruta } = res;
         return `
           <li class="list-group-item listext">
             <span class="reddot"></span>
-            <span class="textrs">${nombre}</span>
+            <span class="textrs"><a href="${ruta}">${ruta}<a/></span>
             <span class="flex-spacer"></span>
             <button type="button" class="btn btn-outline-danger" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">

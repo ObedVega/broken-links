dfad15bbc7752d9e1d9fa2b5cf5b296e3f241291
obed vega <vega.obed@gmail.com>
2023-09-02 12:00:14 -0700

    regions

--- js/check.js
+++ js/check.js
@@ -1,8 +1,7 @@
 fetch('https://ipinfo.io/json')
 .then(response => response.json())
 .then(data => {
-  console.log('Tu dirección IP pública es: ' + data.ip);
-  console.log('Tu ciudad es: ' + data.city);
+  console.log(data.city+', '+data.region+', '+data.region);
 })
 .catch(error => {
   console.error('Error al obtener la información de geolocalización: ' + error);

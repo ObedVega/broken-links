93c1d4c18b0bf140f4e937e482fa55f008002e4f
obed vega <vega.obed@gmail.com>
2023-09-02 11:54:05 -0700

    checkip

--- js/scripts.js
+++ js/scripts.js
@@ -119,3 +119,14 @@ async function startLoading() {
       console.error(error);
     });
   }
+
+
+  fetch('https://ipinfo.io/json')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Tu dirección IP pública es: ' + data.ip);
+    console.log('Tu ciudad es: ' + data.city);
+  })
+  .catch(error => {
+    console.error('Error al obtener la información de geolocalización: ' + error);
+  });

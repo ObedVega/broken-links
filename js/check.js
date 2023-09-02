140fa8f3202b5467acbd7d7a646546e57b429702
obed vega <vega.obed@gmail.com>
2023-09-02 12:02:15 -0700

    ciudad

--- js/check.js
+++ js/check.js
@@ -1,7 +1,7 @@
 fetch('https://ipinfo.io/json')
 .then(response => response.json())
 .then(data => {
-  console.log(data.city+', '+data.region+', '+data.region);
+  console.log(data.city+', '+data.region+', '+data.country);
 })
 .catch(error => {
   console.error('Error al obtener la información de geolocalización: ' + error);

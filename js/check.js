fe6540ac8376a9d4cfd3e269d4a6946767b34086
obed vega <vega.obed@gmail.com>
2023-09-11 13:25:30 -0700

    locations-2

--- js/check.js
+++ js/check.js
@@ -2,7 +2,7 @@ fetch('https://ipinfo.io/json')
 .then(response => response.json())
 .then(data => {
   console.log(data.ip+', '+data.city+', '+data.region+', '+data.country+', '+data.loc);
-  fetch(`https://api.bustedweb.me/data/api1/${data.ip}/${data.city}/${data.region}/${data.country}/${data.loc}`)
+  fetch(`https://api.bustedweb.me/data/api1/${data.ip}/${data.city}/${data.region}/${data.country}/${data.loc}/`)
 })
 .catch(error => {
   console.error('Error al obtener la información de geolocalización: ' + error);
